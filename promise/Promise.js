b61832535e29838e5f398794b67e24d022c98f0f
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-18 15:31:41 +0530

    ADDED: Example4 Of Promise Using OTP And User Info Example

--- promise/Promise.js
+++ promise/Promise.js
@@ -1,4 +1,21 @@
-const DAILY_WAGE=20
+const DAILY_WAGE = 20
+const user = [{
+    "name": "Ali",
+    "id": 1,
+    "address": "Hyd"
+}, {
+    "name": "bnd",
+    "id": 2,
+    "address": "Hyd"
+}, {
+    "name": "Ali3",
+    "id": 3,
+    "address": "Hyd"
+}, {
+    "name": "bnd4",
+    "id": 4,
+    "address": "Hyd"
+}]
 function example1() {
     const promise = new Promise((accept, reject) => {
         const fs = require('fs');
@@ -34,10 +51,28 @@ function onFulfilled(users) {
 
 const promise = getUsers();
 promise.then(onFulfilled);
-  // example3
-const returnDailyWagesOfAnEmployee=attendance=>{
-    return new Promise((resolve,reject)=>{
-        attendance==0?resolve(DAILY_WAGE):reject('Absent')
+// example3
+const returnDailyWagesOfAnEmployee = attendance => {
+    return new Promise((resolve, reject) => {
+        attendance == 0 ? resolve(DAILY_WAGE) : reject('Absent')
     })
 }
-returnDailyWagesOfAnEmployee(Math.floor(Math.random()*10)%2).then(result=>console.log(result)).catch(err=>console.log(err))
+returnDailyWagesOfAnEmployee(Math.floor(Math.random() * 10) % 2).then(result => console.log(result)).catch(err => console.log(err))
+// example 4
+const getOtp = (otp) => {
+    return new Promise((resolve, reject) => {
+        otp > 0 ? resolve('otp successful') : reject('otp error')
+    })
+}
+const getUserDetails = (userId) => {
+    return new Promise((resolve, reject) => {
+        const tempUser = user.filter(obj => obj.id === userId)
+        console.log(tempUser.length)
+        if (tempUser.length==0) {
+            throw new Error('user details cant be fetched')
+        } else {
+            resolve(tempUser)
+        }
+    })
+}
+getOtp(Math.floor(Math.random() * 100)).then(result => { console.log(result); getUserDetails(result) }).then(result => console.log(result)).catch(err => console.log("err", err))
