3bb514b6c3002c5fcc52609df0da7fdd9f1d6d58
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-18 12:57:02 +0530

    ADDED: Example2 Of Promises For Sending Users Data After 1 Second

--- promise/Promise.js
+++ promise/Promise.js
@@ -13,4 +13,23 @@ function example1() {
     })
     promise.then(value => console.log(value)).catch(err => console.log(err))
 }
+
 example1()
+//example2
+function getUsers() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve([
+          { username: 'john', email: 'john@test.com' },
+          { username: 'jane', email: 'jane@test.com' },
+        ]);
+      }, 1000);
+    });
+  }
+  
+  function onFulfilled(users) {
+    console.log(users);
+  }
+  
+  const promise = getUsers();
+  promise.then(onFulfilled);
