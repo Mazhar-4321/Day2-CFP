83c0b6377ee42e534081d4fac8f56be562007103
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-19 18:54:31 +0530

    ADDED: Program To Demonstrate ES8 Features

---
+++ es8-features/Main.js
@@ -0,0 +1,33 @@
+const objectEntriesExample = (obj) => {
+    console.log(Object.entries(obj));
+}
+const objectValuesExample = (obj) => {
+    console.log(Object.values(obj));
+}
+const ObjectgetOwnPropertyDescriptorsExample=(obj)=>{
+    console.log(Object.getOwnPropertyDescriptors(obj));
+}
+const trailingCommasExample=(string="hello world",b,c,)=>{
+const array=[1,2,3,]
+const obj={
+    key:"value",
+}
+}
+const stringPadStartExample=(string="hi")=>{
+ 
+ console.log(string.padStart(9,"mazhar"))
+}
+const stringPadEndExample=(string="hi")=>{
+ 
+    console.log(string.padEnd(9,"mazhar"))
+   }
+const obj={
+    name:"mazhar",
+    address:"Hyderabad",
+    phone:"8125629427"
+}
+objectEntriesExample(obj);
+objectValuesExample(obj);
+ObjectgetOwnPropertyDescriptorsExample(obj);
+stringPadStartExample()
+stringPadEndExample()
---
+++ es8-features/readme.md
@@ -0,0 +1,7 @@
+# ES8 Features
+1. ***async and await***
+1. ***Object.entries()***
+1. ***Object.values()***
+1. ***Object.getOwnPropertyDescriptors()***
+1. ***Trailing Commas***
+1. ***string.padEnd() and string.padStart()***
